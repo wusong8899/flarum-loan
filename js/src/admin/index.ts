3bb42bd5a3586dbb86a0af2f796f454e6c267743
wusong8899 <wusong8899@users.noreply.github.com>
2025-09-03 20:12:31 +0900

    Enhance admin and forum entry points to include loan models and update distribution files.

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,7 +1,12 @@
 import app from 'flarum/admin/app';
 import LoanSettingsPage from './components/LoanSettingsPage';
-import '../common/index';
+import LoanPlatform from '../common/models/LoanPlatform';
+import LoanApplication from '../common/models/LoanApplication';
+import LoanVirtualApproval from '../common/models/LoanVirtualApproval';
 
 app.initializers.add('wusong8899-loan', () => {
   app.extensionData.for('wusong8899-loan').registerPage(LoanSettingsPage);
+  app.store.models['loan-platforms'] = LoanPlatform;
+  app.store.models['loan-applications'] = LoanApplication;
+  app.store.models['loan-virtual-approvals'] = LoanVirtualApproval;
 });
--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -1,7 +1,12 @@
 import app from 'flarum/forum/app';
 import LoanApplicationPage from './components/LoanApplicationPage';
-import '../common/index';
+import LoanPlatform from '../common/models/LoanPlatform';
+import LoanApplication from '../common/models/LoanApplication';
+import LoanVirtualApproval from '../common/models/LoanVirtualApproval';
 
 app.initializers.add('wusong8899-loan', () => {
   app.routes['wusong8899.loan'] = { path: '/loan', component: LoanApplicationPage } as any;
+  app.store.models['loan-platforms'] = LoanPlatform;
+  app.store.models['loan-applications'] = LoanApplication;
+  app.store.models['loan-virtual-approvals'] = LoanVirtualApproval;
 });
