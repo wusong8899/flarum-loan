af6318e120424582071cd904757f1c789e8a4375
wusong8899 <wusong8899@users.noreply.github.com>
2025-09-03 19:59:39 +0900

    Update admin entry point to include common styles and update source map file.

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,5 +1,6 @@
 import app from 'flarum/admin/app';
 import LoanSettingsPage from './components/LoanSettingsPage';
+import '../common/index';
 
 app.initializers.add('wusong8899-loan', () => {
   app.extensionData.for('wusong8899-loan').registerPage(LoanSettingsPage);
